200ed5986732c2c1bd81ee4ca78628b002f33188
Maksym Mitkov <maxitan1701@gmail.com>
2022-10-30 23:08:27 +0200

    send remaining files

---
+++ src/main/resources/static/js/shopConfirm.js
@@ -0,0 +1,16 @@
+document.body.addEventListener('click', e => {
+    if (e.target.classList.contains('confBtn')) {
+
+        // alerty.confirm(content, opts, onOk, onCancel)
+        alerty.confirm(
+            'Вы уверены, что хотите заказать этот электромобиль?',
+            {title: 'Подтверждение заказа', cancelLabel: 'Отменить', okLabel: 'Купить'},
+            function(){
+                alerty.toasts('Заказ подтверждён', {place: 'top'})
+            },
+            function() {
+                alerty.toasts('Заказ отменён')
+            }
+        )
+    }
+})
